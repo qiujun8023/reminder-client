f84e4cfb10a3b6f1bf65cff2dd6a69f2a3fe85df
邱俊 <i@qiujun.me>
2017-07-01 10:21:12 +0800

    chore: 细节调整

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import 'whatwg-fetch'
+import _ from 'lodash'
 import url from 'url'
 
 let getLoginUrl = function (config) {
@@ -22,14 +23,14 @@ let handleHttpError = function (status, data) {
   }
 }
 
-export default function (uri, params) {
+let api = async function (uri, params) {
   params = params || {}
   params.method = params.method || 'GET'
 
   // 提交 Cookie
   params.credentials = 'same-origin'
 
-  // 处理 POST参数，POST 使用 JSON
+  // 处理 POST 参数，POST 使用 JSON
   if (['POST', 'PUT'].indexOf(params.method) !== -1) {
     params.headers = params.headers || {}
     if (!(params.body instanceof FormData)) {
@@ -47,25 +48,22 @@ export default function (uri, params) {
   params.query = undefined
 
   let status, data
-  return fetch(uri, params)
-    .then((res) => {
-      status = res.status
-      return res.json()
-    })
-    .then((res) => {
-      data = res
+  try {
+    let res = await fetch(uri, params)
+    status = res.status
+    data = await res.json()
+    if (status >= 400) {
+      throw new Error(data.message)
+    }
+  } catch (err) {
+    if (!_.isObject(data)) {
+      data = {message: err.message}
+    }
+    handleHttpError(status, data)
+    throw err
+  }
 
-      if (status >= 400) {
-        throw new Error(data.message)
-      }
-      return {status, data}
-    })
-    .catch(function (err) {
-      if (typeof data !== 'object') {
-        data = {message: err.message}
-      }
-
-      handleHttpError(status, data)
-      throw err
-    })
+  return {status, data}
 }
+
+export default api
