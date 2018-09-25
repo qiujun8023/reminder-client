b63a5a2c4bb7415a81a4d3bba80a55fc266f9da2
qiujun <i@qiujun.me>
2018-09-25 16:54:14 +0800

    style: 细节修改

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import 'weui/dist/style/weui.css'
 import 'font-awesome/css/font-awesome.min.css'
 
 import Vue from 'vue'
-import App from '@/App.vue'
+import App from '@/App'
 import router from '@/router'
 
 Vue.config.productionTip = false
